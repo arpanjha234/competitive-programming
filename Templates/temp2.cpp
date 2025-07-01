26277efebdcbac205cc2283d4ba0ae6d80388983
Arpan Jha <62328473+arpanjha234@users.noreply.github.com>
2025-07-01 13:14:15 +0530

    added temp2.cpp

---
+++ Templates/temp2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+/* #include <ext/pb_ds/assoc_container.hpp> */
+
+// pragmas for optimization
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,bmi,bmi2,popcnt,lzcnt")
+
+using namespace std;
+/* using namespace __gnu_pbds; */
+
+// aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using str = string;
+
+// constants
+constexpr ld eps = 1e-9;
+constexpr ll mod = 1e9 + 7;
+constexpr ll inf = 2e9;
+
+// macros
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define endl '\n'
+
+//functions
+void solve() {
+
+}
+
+int main() {
+  // fast-IO
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  int t;
+  cin >> t;
+  while (t--) solve();
+  
+  // solve(); // toggle for single test case
+  return 0;
+}
